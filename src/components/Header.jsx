3c94ef90daf00ad89e7d3abbe5b4351117b2368e
Akevka <akevka@yandex.ru>
2020-06-14 16:50:03 +0300

    12333
    Описание 123

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+    const Header = () =>{
+       return <header className='header'>
+           <img src='https://cdn.icon-icons.com/icons2/876/PNG/512/user-round-button_icon-icons.com_68279.png'/>
+        </header>
+    }
+
+    export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+    const Profile = () =>{
+       return <div className='content'>
+           <div className='panda'>
+               <img width='600px' height='300px' src='http://www.radionetplus.ru/uploads/posts/2013-05/1369460629_panda-1.jpeg'/>
+           </div>
+           <div>Ava + description</div>
+           <div>My posts
+               <div>
+                   New Post
+               </div>
+               <div>
+                   post 1
+               </div>
+               <div>
+                   post 2
+               </div>
+           </div>
+       </div>
+    }
+
+    export default Profile;
