75a7a904d6f2180cdad2c97b73af0ba56b22e45d
Akevka <akevka@yandex.ru>
2020-09-28 23:41:42 +0300

    Updates

---
+++ precache-manifest.1923529e9dda706147ae03eaa2e6acee.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35944567b0c927f0daeb6ed158106d4d",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a986ff5dba3660a600a9",
+    "url": "/my-app/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "ba45f576b6ad11cc40ce",
+    "url": "/my-app/static/css/3.27e02926.chunk.css"
+  },
+  {
+    "revision": "d51131e877953838c0e6",
+    "url": "/my-app/static/css/main.cd715d09.chunk.css"
+  },
+  {
+    "revision": "a986ff5dba3660a600a9",
+    "url": "/my-app/static/js/2.eb33f914.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-app/static/js/2.eb33f914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba45f576b6ad11cc40ce",
+    "url": "/my-app/static/js/3.58c329bd.chunk.js"
+  },
+  {
+    "revision": "7453a3245722b80b111d",
+    "url": "/my-app/static/js/4.bbee04ac.chunk.js"
+  },
+  {
+    "revision": "d51131e877953838c0e6",
+    "url": "/my-app/static/js/main.98a80b91.chunk.js"
+  },
+  {
+    "revision": "69053fcff10313daa709",
+    "url": "/my-app/static/js/runtime-main.b86979e7.js"
+  }
+]);
